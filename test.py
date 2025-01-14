c6c587cc6034479a8584dd28833f4e0d758314ba
Smartcrib0 <Smartcrib26@gmail.com>
2025-01-14 11:29:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,38 +3,43 @@ import time
 
 # إعداد GPIO
 GPIO.setmode(GPIO.BCM)
-IN1 = 17  # دبوس التحكم بالموتور
+IN1 = 17  # دبوس للتحكم بالاتجاه الأول
+IN2 = 27  # دبوس للتحكم بالاتجاه الثاني
 
-# إعداد الدبوس كمخرج
+# إعداد الدبابيس كمخارج
 GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
 
-# إعداد PWM على دبوس IN1
+# إعداد السرعة باستخدام PWM (يمكن استخدام IN1 أو IN2)
 pwm = GPIO.PWM(IN1, 100)  # تردد 100 هرتز
 pwm.start(0)  # بدء PWM مع دورة عمل 0% (الموتور متوقف)
 
-# وظيفة لتشغيل الموتور بسرعة معينة
-def set_motor_speed(speed):
-    """
-    تضبط سرعة الموتور
-    :param speed: سرعة الموتور بين 0 و 100
-    """
-    if 0 <= speed <= 10:
-        pwm.ChangeDutyCycle(speed)  # تغيير دورة العمل لتحديد السرعة
-        print(f"الموتور يعمل بسرعة {speed}%")
-    else:
-        print("خطأ: السرعة يجب أن تكون بين 0 و 100.")
+# دالة لتشغيل الموتور للأمام
+def move_forward(speed):
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    pwm.ChangeDutyCycle(speed)
+
+# دالة لتشغيل الموتور للخلف
+def move_backward(speed):
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    pwm.ChangeDutyCycle(speed)
 
 try:
-    while True:
-        # اطلب من المستخدم إدخال السرعة
-        user_speed = int(input(10))
-        set_motor_speed(user_speed)  # ضبط السرعة
+    print("الموتور يتحرك للأمام بسرعة 50%")
+    move_forward(50)  # تشغيل الموتور للأمام بسرعة 50%
+    time.sleep(5)  # الانتظار لمدة 5 ثوانٍ
+
+    print("الموتور يتحرك للخلف بسرعة 75%")
+    move_backward(75)  # تشغيل الموتور للخلف بسرعة 75%
+    time.sleep(5)  # الانتظار لمدة 5 ثوانٍ
 
 except KeyboardInterrupt:
-    print("The User Stopped The Motor")
+    print("تم إيقاف البرنامج بواسطة المستخدم.")
 
 finally:
-    print("Motor Stop")
+    print("إيقاف الموتور")
     pwm.ChangeDutyCycle(0)  # إيقاف الموتور
     pwm.stop()  # إيقاف PWM
     GPIO.cleanup()  # تنظيف إعدادات GPIO
